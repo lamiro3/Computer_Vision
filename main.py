8900a2c9863be8ed3db8906e221e2485f708fb01
R00T@LAM1R0 <lamiro3@naver.com>
2021-07-09 00:36:08 +0900

    영상 속도개선 / 모자이크 토글 기능 추가

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import cv2
+import keyboard
 
 cas_face = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 cas_eye = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-cam = cv2.VideoCapture(0) #내장 웹캠으로 설정
-cam.set(cv2.CAP_PROP_FPS, 60)
+cam = cv2.VideoCapture(0,cv2.CAP_DSHOW) #내장 웹캠으로 설정
+
+temp = 0
 
 while cam.isOpened():
 
@@ -29,23 +31,34 @@ while cam.isOpened():
                 cv2.putText(roi_frame, 'Eye', (int(ix+iw+5),int(iy+ih)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), thickness=2)
                 
                 roi_frame_frame = roi_frame[iy:iy+ih, ix:ix+iw]
+        
+        if keyboard.is_pressed('f'):
+            temp = 1
+            
+        elif keyboard.is_pressed('e'):
+            temp = 2
+            
+        elif keyboard.is_pressed('b'):
+            temp = 0
+        
+        
+        if temp == 1: # f키를 눌렀을 때
+            blur_Face = cv2.resize(roi_frame, dsize=(0, 0), fx= 0.06, fy = 0.06) # 가로/세로 각각 0.06배
+            blur_Face = cv2.resize(blur_Face, (int(w), int(h)), interpolation=cv2.INTER_AREA)
 
-                if key == ord('f'): # f키를 눌렀을 때
-                    blur_Face = cv2.resize(roi_frame, dsize=(0, 0), fx= 0.06, fy = 0.06) # 가로/세로 각각 0.06배
-                    blur_Face = cv2.resize(blur_Face, (int(w), int(h)), interpolation=cv2.INTER_AREA)
+            frame[y:y+h, x:x+w] = blur_Face # 얼굴 영역 만큼을 모자이크
+            cv2.imshow('', frame)
+            
 
-                    frame[y:y+h, x:x+w] = blur_Face # 얼굴 영역 만큼을 모자이크
-                    cv2.imshow('', frame)
+        elif temp == 2: # e키를 눌렀을 때
+            blur_Eyes = cv2.resize(roi_frame_frame,  dsize=(0, 0), fx= 0.03, fy = 0.03) # 가로/세로 각각 0.03배
+            blur_Eyes = cv2.resize(blur_Eyes, (int(iw), int(ih)), interpolation=cv2.INTER_AREA)
 
-                elif key == ord('e'): # e키를 눌렀을 때
-                    blur_Eyes = cv2.resize(roi_frame_frame,  dsize=(0, 0), fx= 0.03, fy = 0.03) # 가로/세로 각각 0.03배
-                    blur_Eyes = cv2.resize(blur_Eyes, (int(iw), int(ih)), interpolation=cv2.INTER_AREA)
+            roi_frame[iy:iy+ih, ix:ix+iw] = blur_Eyes # 눈 영역 만큼을 모자이크
+            cv2.imshow('', frame)
 
-                    roi_frame[iy:iy+ih, ix:ix+iw] = blur_Eyes # 눈 영역 만큼을 모자이크
-                    cv2.imshow('', frame)
-
-                else:
-                    cv2.imshow('', frame)
+        else:
+            cv2.imshow('', frame)
 
         if key == 27: #Esc키를 눌렀을때
             break # 프로그램 종료
